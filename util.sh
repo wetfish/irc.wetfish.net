77e5ab1d3e6a251726904bd2c876517698330aff
Yam <2872995+Yam-s@users.noreply.github.com>
2021-02-24 02:32:48 +0000

    Add acme-dns support

--- util.sh
+++ util.sh
@@ -15,3 +15,22 @@ if [ -z "$name" ] || [ -z "$domain" ] || [ -z "$email" ]; then
     exit
 fi
 
+if [ ! -z "$acmehost" ]; then
+    if [ ! -f /etc/letsencrypt/acme-dns-auth.py ]; then 
+        curl -o /etc/letsencrypt/acme-dns-auth.py https://raw.githubusercontent.com/joohoi/acme-dns-certbot-joohoi/master/acme-dns-auth.py
+        chmod 0700 /etc/letsencrypt/acme-dns-auth.py
+    fi
+    usingAcmeDns=true;
+    else
+    usingAcmeDns=false;
+fi
+
+acquire_certs () {
+    if $usingAcmeDns; then
+        ## Update acme-dns config in hook script in case we've changed it in config.sh..
+        sed -i "1,/.*ACMEDNS_URL.*/{s/.*ACMEDNS_URL.*/ACMEDNS_URL = \"https:\/\/$acmehost\"/;}" /etc/letsencrypt/acme-dns-auth.py
+        certbot certonly --force-renewal --agree-tos --manual-public-ip-logging-ok -m $email --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns --debug-challenges -d ${joined_domains%-d }
+    else
+        certbot certonly --force-renewal --agree-tos --non-interactive --standalone -m $email -d ${joined_domains%-d }
+    fi
+}
