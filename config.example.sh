849c34c6154f251f6f7422e265042a56d4638897
Edwin Pers <pers.edwin@gmail.com>
2023-08-10 16:39:09 +0000

    spoke - rm old configs

--- config.example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Probably shouldn't change user after install.
-name='user'
-# If you change these after install, run update.sh to apply new certs.
-domain=('domain' 'optional' 'domains')
-email='admin@domain.com'
-# Don't include http(s):// or www. in the host.
-acmehost='auth.domain.com'
